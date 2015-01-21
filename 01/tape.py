bbeef3939ce1c829920c380e558f56f24a92d0b0
Mihnea Simian <contact@mesimian.com>
2015-01-21 22:21:48 +0200

    tape - 33% perf - detected o(n^2) ??

--- 01/tape.py
+++ 01/tape.py
@@ -5,23 +5,22 @@ def solution(A):
     # write your code in Python 2.7
     sum_left = [0]
     sum_right = [0]
-    for (ind, elem) in enumerate(A):
-        if ind == len(A) - 1:
-            continue
-        sum_left.append(sum_left[-1] + elem)
+
+    ind = 0
+    while ind != len(A) - 1:
+        sum_left.append(sum_left[-1] + A[ind])
+        ind += 1
+
     ind = len(A) - 1
     while ind != 0:
         sum_right.insert(0, sum_right[0] + A[ind])
         ind -= 1
+
     sum_left.pop(0)
     sum_right.pop()
-    minimum = abs(sum_left[0]) - abs(sum_right[0])
-    min_ind = 0
-    #print sum_left
-    #print sum_right
-    for p in range(len(sum_left)):
-        if minimum > abs(sum_left[p]) - abs(sum_right[p]):
-            minimum = abs(sum_left[p]) - abs(sum_right[p])
-            min_ind = p
-    return min_ind + 1
 
+    minimum = abs(sum_left[0] - sum_right[0])
+    for p in range(len(sum_left)):
+        minimum = min(minimum, abs(sum_left[p] - sum_right[p]))
+
+    return minimum
