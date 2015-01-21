8369082c0958c3bb4f36db6f33de037d7eb39f10
Mihnea Simian <contact@mesimian.com>
2015-01-21 22:29:44 +0200

    what do you know, list.insert was O(n) 100% performance on TapeEquilibrium

--- 01/tape.py
+++ 01/tape.py
@@ -3,24 +3,26 @@
 
 def solution(A):
     # write your code in Python 2.7
-    sum_left = [0]
-    sum_right = [0]
+    sums_left = [0]
+    sums_right = [0]
+    len_a = len(A)
 
     ind = 0
-    while ind != len(A) - 1:
-        sum_left.append(sum_left[-1] + A[ind])
+    while ind != len_a - 1:
+        sums_left.append(sums_left[-1] + A[ind])
         ind += 1
 
-    ind = len(A) - 1
+    ind = len_a - 1
     while ind != 0:
-        sum_right.insert(0, sum_right[0] + A[ind])
+        sums_right.append(sums_right[-1] + A[ind])
         ind -= 1
 
-    sum_left.pop(0)
-    sum_right.pop()
+    sums_left.pop(0)
+    sums_right.pop(0)
 
-    minimum = abs(sum_left[0] - sum_right[0])
-    for p in range(len(sum_left)):
-        minimum = min(minimum, abs(sum_left[p] - sum_right[p]))
+    sums_len = len_a - 1
+    minimum = abs(sums_left[0] - sums_right[sums_len - 1])
+    for p in range(sums_len):
+        minimum = min(minimum, abs(sums_left[p] - sums_right[sums_len - 1 - p]))
 
     return minimum
